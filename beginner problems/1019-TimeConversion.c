e5eda91463910b6a193ab1ad969bd2f77f168289
Asatter833 <asatter833@gmail.com>
2022-12-15 21:11:24 +0600

    Problem Solving Day-5

--- beginner problems/1019-TimeConversion.c
+++ beginner problems/1019-TimeConversion.c
@@ -2,4 +2,11 @@
 
 int main()
 {
+    int main_time, hour, minute, second;
+    scanf("%d", &main_time);
+    hour = main_time / 3600;
+    main_time = main_time % 3600;
+    minute = main_time / 60;
+    second = main_time % 60;
+    printf("%d:%d:%d\n", hour, minute, second);
 }
