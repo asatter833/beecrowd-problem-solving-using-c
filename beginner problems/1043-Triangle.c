6b6073af5806a22c4d9aa7ddcebbbb24321d0400
Asatter833 <asatter833@gmail.com>
2022-12-17 19:56:31 +0600

    Problem Solving Day-8

---
+++ beginner problems/1043-Triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float A, B, C;
+    scanf("%f%f%f", &A, &B, &C);
+    if (A + B > C && B + C > A && A + C > B)
+    {
+        printf("Perimetro = %.1f\n", (A + B + C));
+    }
+    else
+    {
+        printf("Area = %.1f\n", (((A + B) / 2) * C));
+    }
+}
