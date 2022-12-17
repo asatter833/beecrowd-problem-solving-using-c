2e9aa0df46fcf1beebe40273df74e3979f049e73
Asatter833 <asatter833@gmail.com>
2022-12-17 20:21:38 +0600

    Problem Solving Day-8

--- beginner problems/1044-Multiples.c
+++ beginner problems/1044-Multiples.c
@@ -2,4 +2,10 @@
 
 int main()
 {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a % b == 0 || b % a == 0)
+        printf("Sao Multiplos\n");
+    else
+        printf("Nao sao Multiplos\n");
 }
