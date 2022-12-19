b2b5c593da12558647df96ce5c01ec5db262e23f
Asatter833 <asatter833@gmail.com>
2022-12-20 00:11:15 +0600

    Problem Solving Day-10

--- beginner problems/1047-GameTimeWithMinutes.c
+++ beginner problems/1047-GameTimeWithMinutes.c
@@ -2,10 +2,13 @@
 
 int main()
 {
-    int start_hour, start_min, end_hour, end_min;
+    int start_hour, start_min, end_hour, end_min, diff;
     scanf("%d%d%d%d", &start_hour, &start_min, &end_hour, &end_min);
-    if (start_hour == end_hour && start_min == end_min)
+
+    diff = ((end_hour * 60) + end_min) - ((start_hour * 60) + start_min);
+    if (diff <= 0)
     {
-        printf("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+        diff += (24 * 60);
     }
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", (diff / 60), (diff % 60));
 }
