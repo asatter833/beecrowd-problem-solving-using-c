64d6208867bf994e0c8c79c0b4078b6f94f6b3ab
Asatter833 <asatter833@gmail.com>
2023-01-11 12:29:37 +0600

    Problem Solving Day-18

---
+++ beginner problems/1094-Experiments.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int N, quantity, qC = 0, qR = 0, qS = 0, i, total;
+    double pC, pR, pS;
+    char Ctype;
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++)
+
+    {
+        scanf("%d %c", &quantity, &Ctype);
+        if ('C' == Ctype)
+        {
+            qC = qC + quantity;
+        }
+        if ('R' == Ctype)
+        {
+            qR = qR + quantity;
+        }
+        if ('S' == Ctype)
+        {
+            qS = qS + quantity;
+        }
+    }
+    total = qC + qR + qS;
+    pC = ((qC / (total * 1.0)) * 100.00);
+    pR = ((qR / (total * 1.0)) * 100.00);
+    pS = ((qS / (total * 1.0)) * 100.00);
+
+    printf("Total: %d cobaias\n", total);
+    printf("Total de coelhos: %d\n", qC);
+    printf("Total de ratos: %d\n", qR);
+    printf("Total de sapos: %d\n", qS);
+    printf("Percentual de coelhos: %.2lf %%\n", pC);
+    printf("Percentual de ratos: %.2lf %%\n", pR);
+    printf("Percentual de sapos: %.2lf %%\n", pS);
+}
