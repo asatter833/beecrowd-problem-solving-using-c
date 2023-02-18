020abb93332c0a1e8ffdd2402cf1e7a30540ac55
Asatter833 <asatter833@gmail.com>
2023-02-18 13:41:37 +0600

    Problem Solving Day-25

---
+++ beginner problems/1117-ScoreValidation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    float score, media, total = 0;
+    int m = 0;
+    while (1)
+    {
+        scanf("%f", &score);
+        if (score < 0 || score > 10)
+        {
+            printf("nota invalida\n");
+        }
+        else
+        {
+            m++;
+            total = score + total;
+            if (m == 2)
+            {
+                media = (total / 2);
+                printf("media = %.2f\n", media);
+                break;
+            }
+        }
+    }
+}
