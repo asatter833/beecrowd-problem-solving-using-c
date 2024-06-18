069ba2e5a40858dfb23a46d5f6c19edbca23265e
abdus satter <asatter833@gmail.com>
2024-06-18 20:58:19 +0600

    Formatter for c

--- beginner problems/1132-MultiplesOf13.c
+++ beginner problems/1132-MultiplesOf13.c
@@ -3,27 +3,24 @@
 int main()
 {
     int number1, number2, sum = 0, i;
-    scanf("%d%d",&number1, &number2);
-    if (number1<=number2)
+    scanf("%d%d", &number1, &number2);
+    if (number1 <= number2)
     {
-        for(i=number1; i<=number2; i++)
+        for (i = number1; i <= number2; i++)
         {
-          if(i%13 != 0)
-          sum = sum+i;
+            if (i % 13 != 0)
+                sum = sum + i;
         }
-       printf("%d\n", sum);
-
+        printf("%d\n", sum);
     }
-    else {
-         {
-        for(i=number2; i<=number1; i++)
+    else
+    {
+
+        for (i = number2; i <= number1; i++)
         {
-          if(i%13 != 0)
-          sum = sum+i;
+            if (i % 13 != 0)
+                sum = sum + i;
         }
-       printf("%d\n", sum);
-
-    }
+        printf("%d\n", sum);
     }
 }
-    
